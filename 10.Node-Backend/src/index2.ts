4d747be06d16bf5707ac8d0ca8e0be159ffc4997
Alejandro Cerezo <alce65@hotmail.es>
2026-03-23 14:10:02 +0100

    Add statics server to backend

---
+++ 10.Node-Backend/src/index2.ts
@@ -0,0 +1,82 @@
+import { createServer, IncomingMessage, ServerResponse } from 'node:http';
+import { readFile } from 'node:fs/promises';
+import serveStaticCreator from 'serve-static';
+import debugCreator from 'debug';
+
+const debug = debugCreator('10-back');
+
+const PORT = Number(process.env.PORT) || 3000;
+
+const notes = [
+    {
+        id: '1',
+        content: 'Primera nota',
+    },
+    {
+        id: '2',
+        content: 'Segunda nota',
+    },
+    {
+        content: 'Nota creada',
+        id: 'df10700f-18ae-41ea-9e8e-16583023b8db',
+        owner: 'Pepe',
+    },
+];
+
+const serveStatic = serveStaticCreator('./public', {});
+
+const app = async (request: IncomingMessage, response: ServerResponse) => {
+    try {
+        // console.log(request.url, request.method);
+        // console.log(request.headers);
+        debug(`[${request.method}] ${request.url}`);
+
+        if (request.method === 'PUT') {
+            response.statusCode = 405;
+            response.end();
+            return;
+        }
+
+        if (request.method === 'POST') {
+            response.statusCode = 201;
+        }
+
+        let html = '';
+        switch (request.url) {
+            case '/api':
+                response.setHeader('Content-type', 'text/html; charset=utf-8');
+                response.end('<p>API Rest</p>');
+                break;
+            case '/notes':
+                response.setHeader('Content-type', 'application/json');
+                // Así no funcionaría response.write(notes);
+                response.write(JSON.stringify(notes));
+                response.end();
+                break;
+            default:
+                response.statusCode = 200;
+                html = await readFile('./public/index.html', {
+                    encoding: 'utf-8',
+                });
+                response.end(html);
+                break;
+        }
+
+        return;
+    } catch (error) {
+        console.error((error as Error).message);
+    }
+};
+
+const middleware = (request: IncomingMessage, response: ServerResponse) => {
+    serveStatic(request, response, () => {
+        debug(`[${request.method}] ${request.url}`);
+        app(request, response);
+    });
+};
+
+const server = createServer(middleware);
+
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
