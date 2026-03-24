40ddb53f7caf4c373e2b4b64d1ff9054e20f24f0
Alejandro Cerezo <alce65@hotmail.es>
2026-03-24 14:08:10 +0100

    Update front components from previous projects

---
+++ 10.Front/src/core/components/header/header.ts
@@ -0,0 +1,45 @@
+import { Theme } from '../theme.ts';
+
+export class Header extends HTMLElement {
+    static #selector = 'app-header';
+    static render() {
+        document
+            .querySelectorAll(Header.#selector)
+            .forEach((el) => el.replaceWith(new Header()));
+        Theme.render();
+    }
+
+    #template!: string;
+
+    constructor() {
+        super()
+        this.setTemplate();
+        this.setElement()
+    }
+
+    setTemplate() {
+        this.#template =
+            /*html*/
+            `<header class="header">
+        <nav>
+            <app-menu data-type="mobile-menu"></app-menu>
+            <app-menu data-type="full-menu"></app-menu>
+        </nav>
+        <dialog class="menu-dialog" id="menu-dialog">
+            <nav>
+                <app-menu></app-menu>
+            </nav>
+        </dialog>
+        <app-theme></app-theme>
+    </header>
+    `;
+    }
+
+    setElement() {
+        // const parentElement = document.createElement('parent');
+        // parentElement.innerHTML = template;
+        // const element = parentElement.firstElementChild;
+        // return element;
+        this.innerHTML = this.#template
+    }
+}
