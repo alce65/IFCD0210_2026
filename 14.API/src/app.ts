a87d79248f9f16f858c734312a7d0d598d915ff1
Alejandro Cerezo <alce65@hotmail.es>
2026-04-17 14:06:31 +0200

    Add initial animals router

--- 14.API/src/app.ts
+++ 14.API/src/app.ts
@@ -1,15 +1,44 @@
 import { env } from "./config/env.ts";
 import debug from 'debug';
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
 import type { Pool } from "pg";
+import { animalRouter } from "./animals/routers/animal.router.ts";
 
 
 export const createApp = (pool: Pool) => {
     const log = debug(`${env.PROJECT_NAME}:app`);
     log("Starting Express app...");
     const app = express();
+    app.disable('x-powered-by');
+    // Middleware Utilities
+    app.use(morgan('dev'));
+    app.use(
+        cors({
+            origin: '*',
+        }),
+    );
+    app.use(express.json());
+    app.use(express.urlencoded());
 
-    log(pool.ending)
+    app.use('/health', (_req, res) => {
+        return res.json({
+            status: 'ok',
+            timestamp: new Date().toISOString(),
+        });
+    });
 
+    app.use('/api/animals', animalRouter(pool));
+
+    app.use((_req, res) => {
+        res.status(404);
+        res.statusMessage = 'Not Found';
+        return res.json({
+            message: 'Resource not found',
+        });
+    });
+    
     return app;
 }
+
